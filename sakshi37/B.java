9bf82a3fdd97a240c9807ed1f19825a2513643a1
Sakshi-Nik <sakshinikam266@gmail.com>
2025-03-16 22:10:59 -0700

    Add files via upload

---
+++ sakshi37/B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class A
+{
+ void get()
+{
+ System.out.println("hello");
+}
+}
+
+
+
+class B
+{
+ public static void main(String args[])
+{
+
+A a1 =  new A();
+a1.get();
+}
+}
---
+++ sakshi37/New.txt
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class New
+{
+public static void main(String args[])
+{
+ System.out.println("hi...");
+}
+}
+
+
